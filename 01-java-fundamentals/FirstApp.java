cd8abb738e93f825d9bc51b961ac75f4d2ef3dd8
Stuart-Yee <ki6fzb@gmail.com>
2021-06-07 21:34:25 -0700

    first commit

---
+++ 01-java-fundamentals/FirstApp.java
@@ -0,0 +1,14 @@
+public class FirstApp{
+    /* My name is Coding Dojo
+I am 100 years old
+My hometown is Burbank, CA
+*/
+
+    public void printMsg(){
+        System.out.println("My name is Stuart Yee");
+        System.out.println("I am 40 years old");
+        System.out.println("My hometown is San Francisco, CA");
+    }
+
+
+}
---
+++ 01-java-fundamentals/FirstAppTest.java
@@ -0,0 +1,6 @@
+public class FirstAppTest{
+    public static void main(String[] args){
+        FirstApp messageObj = new FirstApp();
+        messageObj.printMsg(); 
+    }
+}
