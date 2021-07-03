e45df48526d728bfca705cb4a5ddc2a86e3c098b
Stuart-Yee <ki6fzb@gmail.com>
2021-07-02 22:15:55 -0700

    Books Show assignment

---
+++ 03-java-spring/02-spring-data-i/02-show/Books/src/main/java/com/stuartyee/books/services/BookService.java
@@ -0,0 +1,54 @@
+package com.stuartyee.books.services;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import com.stuartyee.books.models.Book;
+import com.stuartyee.books.repositories.BookRepository;
+
+@Service
+public class BookService {
+ // adding the book repository as a dependency
+	private BookRepository bookRepository;
+	
+	private List<Book> books = new ArrayList(Arrays.asList(
+            new Book("Harry Potter and the Sorcerer's Stone", "A boy wizard saving the world", "english", 309),
+            new Book("The Great Gatsby", "The story primarily concerns the young and mysterious millionaire Jay Gatsby", "english", 180),
+            new Book("Moby Dick", "The saga of Captain Ahab", "english", 544),
+            new Book("Don Quixote", "Life of a retired country gentleman", "english", 150),
+            new Book("The Odyssey", "Ancient Greek epic poem", "english", 475)
+            ));
+ 
+	public BookService(BookRepository bookRepository) {
+		this.bookRepository = bookRepository;
+	}
+ // returns all the books
+	public List<Book> allBooks() {
+		return bookRepository.findAll();
+	}
+ // creates a book
+	public Book createBook(Book b) {
+		return bookRepository.save(b);
+	}
+ // retrieves a book
+	public Book findBook(Long id) {
+		Optional<Book> optionalBook = bookRepository.findById(id);
+		if(optionalBook.isPresent()) {
+			return optionalBook.get();
+		} else {
+			return null;
+		}
+	}
+	
+    public Book findBookByIndex(int index) {
+        if (index < books.size()){
+            return books.get(index);
+        }else{
+            return null;
+        }
+    }
+}
+
