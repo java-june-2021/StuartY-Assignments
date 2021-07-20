265376b9b287075b2b6745c9ac33f192e501c068
Stuart-Yee <ki6fzb@gmail.com>
2021-07-19 22:16:00 -0700

    Login/Reg without validations

---
+++ 03-java-spring/05-login-and-registration/01-controller-and-views/LoginReg/src/main/java/com/stuartyee/loginreg/controllers/Users.java
@@ -0,0 +1,78 @@
+package com.stuartyee.loginreg.controllers;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.stuartyee.loginreg.models.User;
+import com.stuartyee.loginreg.services.UserService;
+
+//imports removed for brevity
+@Controller
+public class Users {
+	private final UserService userService;
+ 
+	public Users(UserService userService) {
+		this.userService = userService;
+	}
+ 
+	@RequestMapping("/registration")
+	public String registerForm(@ModelAttribute("user") User user) {
+		return "registrationPage.jsp";
+	}
+
+	@RequestMapping("/login")
+	public String login() {
+		return "loginPage.jsp";
+	}
+ 
+	@RequestMapping(value="/registration", method=RequestMethod.POST)
+	public String registerUser(@Valid @ModelAttribute("user") User user, BindingResult result, HttpSession session) {
+		if(result.hasErrors()) {
+			return "registrationPage.jsp";// if result has errors, return the registration page (don't worry about validations just now)
+		} else {
+			userService.registerUser(user);// else, save the user in the database, save the user id in session, and redirect them to the /home route
+			session.setAttribute("id", user.getId());
+			return "redirect:/home";
+		}
+	 	
+     
+	}
+ 
+	@RequestMapping(value="/login", method=RequestMethod.POST)
+	public String loginUser(@RequestParam("email") String email, @RequestParam("password") String password, Model model, HttpSession session) {
+		
+		if(userService.authenticateUser(email, password)) {
+			User user = userService.findByEmail(email);
+			session.setAttribute("id", user.getId());
+			return "redirect:/home";
+		} else {
+			model.addAttribute("error", "Apologies but it looks like this is an incorrect email/password combination.");
+			return "loginPage.jsp";
+		}
+		// if the user is authenticated, save their user id in session
+		// else, add error messages and return the login page
+	}
+ 
+	@RequestMapping("/home")
+	public String home(HttpSession session, Model model) {
+		Long id = (Long)session.getAttribute("id");
+		model.addAttribute("user", userService.findUserById(id));
+		return "homePage.jsp";
+		// get user from session, save them in the model and return the home page
+	}
+ 
+	@RequestMapping("/logout")
+	public String logout(HttpSession session) {
+		session.invalidate();// invalidate session
+		return "redirect:/login";// redirect to login page
+	}
+
+}
